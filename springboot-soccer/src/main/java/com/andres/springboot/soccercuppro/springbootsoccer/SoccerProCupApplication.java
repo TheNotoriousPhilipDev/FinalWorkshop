5214e8c444f0b772e6bec6ca2e826edea16862f1
Felipe <afgomez0622@gmail.com>
2024-05-07 18:28:53 -0500

    Spring boot structure added

---
+++ springboot-soccer/src/main/java/com/andres/springboot/soccercuppro/springbootsoccer/SoccerProCupApplication.java
@@ -0,0 +1,13 @@
+package com.andres.springboot.soccercuppro.springbootsoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerProCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerProCupApplication.class, args);
+	}
+
+}
